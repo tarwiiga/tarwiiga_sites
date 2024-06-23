ffb3a1b840129ebd9bdc0a243f93e52904500a1d
maelghrib <maelghrib@gmail.com>
2024-06-23 21:42:39 +0300

    Added the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import React from "react";
+import {Inter} from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Tarwiiga Sites",
-  description: "Use AI to generate websites in seconds.",
+    title: "Tarwiiga Sites",
+    description: "Use AI to generate websites in seconds.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang="en">
+        <head>
+            <link rel="icon" href="/favicon.ico" sizes="100x100"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
