36b8686446b2de807c689c3163dffc62b961ee71
maelghrib <maelghrib@gmail.com>
2024-06-24 05:49:24 +0300

    Added template

---
+++ src/app/template/data.ts
@@ -0,0 +1,37 @@
+import {PageType} from "@/utils/types";
+
+export const page1: PageType = {
+    logoText: "",
+    title: "",
+    link: "",
+    headline: "",
+    description: "",
+    phone_number: "",
+    bgColor: "#ffffff",
+    textColor: "#000000",
+    pages: [],
+}
+
+export const page2: PageType = {
+    logoText: "",
+    title: "",
+    link: "",
+    headline: "",
+    description: "",
+    phone_number: "",
+    bgColor: "#ffffff",
+    textColor: "#000000",
+    pages: [],
+}
+
+export const home_page: PageType = {
+    logoText: "",
+    title: "",
+    link: "",
+    headline: "",
+    description: "",
+    phone_number: "",
+    bgColor: "#ffffff",
+    textColor: "#000000",
+    pages: [page1, page2],
+}
