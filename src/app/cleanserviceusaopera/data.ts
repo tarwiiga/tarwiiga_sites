52868b49c1375270dbf45a47f7288ae230dce72b
maelghrib <maelghrib@gmail.com>
2024-06-25 01:46:07 +0000

    Deployed Clean Service USA Opera to https://sites.tarwiiga.com/cleanserviceusaopera

---
+++ src/app/cleanserviceusaopera/data.ts
@@ -0,0 +1,37 @@
+
+    import {PageType, SiteType} from "@/utils/types";
+
+    export const page1: PageType = {
+        title: "USA Services Opera",
+        link: "/cleanserviceusaopera/usa-services-opera",
+        headline: "Our USA Services Opera",
+        description: "We, at USA Opera, offer a wide range of cleaning services to meet your needs.",
+        phone_number: "0123456789011",
+    }
+
+    export const page2: PageType = {
+        title: "USA Contact Opera",
+        link: "/cleanserviceusaopera/usa-contact-opera",
+        headline: "Contact Us in USA Today Opera",
+        description: "Get in touch with us in USA Opera today to schedule a cleaning.",
+        phone_number: "0123456789011",
+    }
+
+    export const home_page: PageType = {
+        title: "Home",
+        link: "/cleanserviceusaopera",
+        headline: "Clean Service USA Opera",
+        description: "Get Your Home Sparkling Clean USA Opera",
+        phone_number: "0123456789011",
+    }
+
+    export const site: SiteType = {
+        name: "Clean Service USA Opera",
+        domain: "cleanserviceusaopera",
+        bg_color: "#ffffff",
+        text_color: "#000000",
+        home_page: home_page,
+        page1: page1,
+        page2: page2,
+    }
+    
